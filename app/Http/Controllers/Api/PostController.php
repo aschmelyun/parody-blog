6dc57d733508529016fa6384716907f7da806007
Andrew Schmelyun <schmelyun@gmail.com>
2021-04-03 19:47:36 -0400

    Adds index method to PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,8 +4,15 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    
+    public function index()
+    {
+        return Post::with('user')
+            ->get();
+    }
+
 }
