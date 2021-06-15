5f6f6b13ad55ba9cf8a91b34744e5e9d0a93e1b9
Andrew Schmelyun <schmelyun@gmail.com>
2021-06-15 03:33:06 -0400

    Adds global ordering by created_at desc to Comment and Post models

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,11 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function(Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
 
     public function user()
     {
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
@@ -10,6 +11,14 @@ class Post extends Model
     use HasFactory;
 
     protected $appends = ['comment_count'];
+
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope('order', function(Builder $builder) {
+            $builder->orderBy('created_at', 'desc');
+        });
+    }
 
     public function getRouteKeyName()
     {
